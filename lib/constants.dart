9becabc4f83ac6e25bbf4fbc7ce05ba7c47abb88
0xAhmd <ahmedhesham200987@gmail.com>
2025-08-04 11:41:47 +0300

    project setup & splash screen

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color background = Color(0xFF040C23);
+const Color text = Color(0xFFA19CC5);
+const Color orange = Color(0xFFF9B091);
+const Color primary = Color(0xFFA44AFF);
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:azkar/splash/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+  }
+}
