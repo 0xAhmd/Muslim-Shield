8f4049ede3951d1e827fbfe2e0c36bb15f5ebfc2
0xAhmd <ahmedhesham200987@gmail.com>
2025-08-05 16:49:52 +0300

    presentation layer

--- lib/home/presentation/pages/home_screen.dart
+++ lib/home/presentation/pages/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:azkar/home/presentation/pages/main_tab.dart';
 import 'package:azkar/home/presentation/widgets/custom_nav_bar.dart';
 import 'package:azkar/prayer/presentation/pages/prayer_page.dart';
 import 'package:flutter/material.dart';
-import 'package:azkar/Doa/presentation/doa_page.dart';
+import 'package:azkar/Doa/presentation/pages/doa_page.dart';
 import 'package:azkar/Reminders/presentation/reminders_page.dart';
 import 'package:azkar/bookmarks/presentation/bookmarks_page.dart';
 
