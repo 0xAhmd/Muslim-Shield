1bc86016f0ac84d5c1f5f8f5f793906077bd37af
0xAhmd <ahmedhesham200987@gmail.com>
2025-08-05 11:06:49 +0300

    juzz state defined

---
+++ lib/juzz/presentation/cubit/juzz_state.dart
@@ -0,0 +1,56 @@
+import 'package:azkar/juzz/data/models/juzz.dart';
+import 'package:azkar/juzz/data/models/juzz_summary.dart';
+
+
+// States
+abstract class JuzzState {}
+
+class JuzzInitial extends JuzzState {}
+
+class JuzzLoading extends JuzzState {}
+
+class JuzzLoaded extends JuzzState {
+  final List<JuzzSummary> juzzSummaries;
+  final List<JuzzSummary> filteredJuzz;
+  final String searchQuery;
+
+  JuzzLoaded({
+    required this.juzzSummaries,
+    required this.filteredJuzz,
+    this.searchQuery = '',
+  });
+
+  JuzzLoaded copyWith({
+    List<JuzzSummary>? juzzSummaries,
+    List<JuzzSummary>? filteredJuzz,
+    String? searchQuery,
+  }) {
+    return JuzzLoaded(
+      juzzSummaries: juzzSummaries ?? this.juzzSummaries,
+      filteredJuzz: filteredJuzz ?? this.filteredJuzz,
+      searchQuery: searchQuery ?? this.searchQuery,
+    );
+  }
+}
+
+class JuzzError extends JuzzState {
+  final String message;
+
+  JuzzError(this.message);
+}
+
+class JuzzDetailLoading extends JuzzState {}
+
+class JuzzDetailLoaded extends JuzzState {
+  final Juzz juzz;
+  final List<JuzzSummary> juzzSummaries; // Keep the list for navigation
+
+  JuzzDetailLoaded({required this.juzz, required this.juzzSummaries});
+}
+
+class JuzzDetailError extends JuzzState {
+  final String message;
+  final List<JuzzSummary> juzzSummaries; // Keep the list for navigation
+
+  JuzzDetailError({required this.message, required this.juzzSummaries});
+}
