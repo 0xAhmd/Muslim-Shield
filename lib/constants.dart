d8d449ca9ce1bf9f71828569eb6ca0e303f8e056
0xAhmd <ahmedhesham200987@gmail.com>
2025-08-06 08:29:24 +0300

    change pallete

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
-const Color background = Color(0xFF040C23);
+const Color background = Color.fromARGB(255, 3, 10, 29);
 const Color textColor = Color(0xFFA19CC5);
 const Color orange = Color(0xFFF9B091);
 const Color primary = Color(0xFFA44AFF);
-const Color gray = Color(0xFF121931);
+const Color gray = Color.fromARGB(255, 3, 10, 29);
