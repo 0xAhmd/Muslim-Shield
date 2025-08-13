2d6d6e9423e274e827bddde5e462dd632c9237b3
0xAhmd <ahmedhesham200987@gmail.com>
2025-08-13 13:10:20 +0300

    const

--- lib/surah/pages/surah_details_page.dart
+++ lib/surah/pages/surah_details_page.dart
@@ -330,7 +330,7 @@ class _SurahDetailScreenState extends State<SurahDetailScreen>
         elevation: 0,
         leading: IconButton(
           onPressed: () => Navigator.pop(context),
-          icon: Icon(Icons.arrow_back_outlined, color: primary),
+          icon: const Icon(Icons.arrow_back_outlined, color: primary),
         ),
         title: Text(
           widget.surah.englishName,
